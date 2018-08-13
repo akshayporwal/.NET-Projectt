f79d3a5c1129388f8c0736aae8c9e2f5a6192d43
Akshay Porwal <akiporwal99@gmail.com>
2018-08-13 15:31:07 +0530

    webapi
    Webapi

---
+++ WebApiMVC/WebApiMVC/Models/Customer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApiMVC.Models
+{
+    public class Customer
+    {
+
+        [Display(Name = "CustomerId")]
+        public int CustomerId { get; set; }
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+        [Display(Name = "Address")]
+        public string Address { get; set; }
+        [Display(Name = "MobileNo")]
+        public string MobileNo { get; set; }
+        [Display(Name = "Birthdate")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
+        //[Display(Prompt = "MM/DD/YYYY")]
+        [DataType(DataType.Date)]
+        public DateTime Birthdate { get; set; }
+        [Display(Name = "EmailId")]
+        public string EmailId { get; set; }
+    }
+}
